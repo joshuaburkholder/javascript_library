0976db09e83afe2b33496ebb8aeb5ea42ecf4dbb
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-05-28 17:22:28 -0400

    adding pagination

--- JS_Quiz/main.js
+++ JS_Quiz/main.js
@@ -6,6 +6,12 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
 const myQuestions = [
 	{
 		question: "Who is the strongest?",
@@ -59,8 +65,10 @@ function buildQuiz(){
 			}
 
 			output.push(
-				`<div class="question"> ${currentQuestion.question} </div>
-				 <div class="answers">${answers.join('')} </div>`
+				`<div class="slide">
+				<div class="question"> ${currentQuestion.question} </div>
+				 <div class="answers">${answers.join('')} </div>
+				 </div>`
 			);
 		});
 
@@ -93,6 +101,39 @@ function showResults(){
 	resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
 }
 
+function showSlide(n){
+	slides[currentSlide].classList.remove('active-slide');
+	slides[n].classList.add('active-slide');
+	currentSlide = n;
+
+	if(currentSlide === 0){
+		previousButton.style.display = 'none';
+	}
+	else {
+		previousButton.style.display = 'inline-block';
+	}
+	if(currentSlide === slide.length-1){
+		nextButton.style.display = 'none';
+		submitButton.style.display = 'inline-block';
+	}
+	else {
+		nextButton.style.display = 'inline-block';
+		submitButton.style.display = 'none';
+	}
+}
+
+showSlide(0);
+
+function showNextSlide(){
+	showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide(){
+	showSlide(currentSlide - 1);
+}
+
+previousButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
 
 //display quiz right away
 buildQuiz();
